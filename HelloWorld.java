2164b445d6ab684e4daaa0c42271bcd97bcac09c
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-12-17 17:27:22 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,24 @@
-package com.annotation;
+package com.beans;
 
 public class HelloWorld {
-private String message;
-
-public String getMessage() {
-	return message;
-}
-
-public void setMessage(String message) {
-	this.message = message;
-}
-
+	private String message;
+	HelloWorld()
+	{
+		System.out.println("Helloworld Bean initialized");
+	}
+	public HelloWorld(String message)
+	{
+		this.message=message;
+	}
+	void display()
+	{
+		System.out.println("Welcome to Spring");
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
 }
