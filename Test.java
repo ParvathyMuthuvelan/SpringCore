2f21313aad57874b3a07f22637e384fa083195f3
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-12-18 10:21:22 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -8,13 +8,47 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
-
-		public static void main(String[] args) throws BeansException, FileNotFoundException {
-			ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-			HelloWorld obj1 = (HelloWorld) context.getBean("hello");
-obj1.display();
+	public static void main(String[] args) throws BeansException, FileNotFoundException {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		System.out.println("Bean scope");
+		HelloWorld obj1 = (HelloWorld) context.getBean("hello");
+		System.out.println(obj1.getMessage());
+		System.out.println("Hashcode of obj1="+obj1.hashCode());
+		HelloWorld obj2 = (HelloWorld) context.getBean("hello");
+		System.out.println("Hashcode of obj2="+obj2.hashCode());
 	
+		HelloWorld obj3 = (HelloWorld) context.getBean("hello");
+		System.out.println("Hashcode of obj3="+obj3.hashCode());
 
+		
+		
+		
+		//constructor injection (3-arg cons)
+		System.out.println("Constructor injection");
+		Player p=(Player)context.getBean("player");
+		System.out.println("Player details");
+		System.out.println(p);
+		
+		//constructor-injection(2-arg cons)
+		Player p1=(Player)context.getBean("player1");
+		System.out.println("Player details");
+		System.out.println(p1);
+		
+		//constructor-injection(2-arg cons - index)
+				Player p2=(Player)context.getBean("player2");
+				System.out.println("Player details");
+				System.out.println(p2);
+				System.out.println("Inner bean - cons injection");
+				Person person1=(Person)context.getBean("person1");
+				System.out.println(person1);
+				System.out.println("Inner bean - setter injection");
+				Person person2=(Person)context.getBean("person2");
+				System.out.println(person2);
+				
+				Employee emp=(Employee)context.getBean("emp");
+				System.out.println(emp);
+				
+				
 	}
 
 }
