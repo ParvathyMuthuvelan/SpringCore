298e429846750a0412fb24cf2b4f8b54668c8af7
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-12-18 10:56:40 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,26 +1,33 @@
-package com.spring.aop;
+package com.beans;
 
 public class Employee {
-    private int id;
+	private int id;
 	private String name;
-
+	private Address address;
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public void throwException() {
-		throw new RuntimeException("Dummy Exception");
+	
+	public Address getAddress() {
+		return address;
 	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return String.format("Employee [id=%s, name=%s, address=%s]", id, name, address);
+	}
+
+	
+
 }
