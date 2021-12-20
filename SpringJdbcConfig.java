6b370e73aa0381bdba14646361a738755e5178ad
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-12-20 17:00:30 +0530

    Add files via upload

--- SpringJdbcConfig.java
+++ SpringJdbcConfig.java
@@ -2,9 +2,12 @@ package com.springjdbc;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -13,33 +16,45 @@ import com.dao.PersonDaoImpl;
 import com.service.PersonServiceImpl;
 
 @Configuration
-@ComponentScan("com.dao.*,com.service.*")
+//@ComponentScan("com.dao.*,com.service.*")
+@PropertySource("classpath:db.properties")
 public class SpringJdbcConfig {
+	@Autowired
+	Environment environment;
+	private final String URL = "db.url";
+	private final String USER = "db.username";
+	private final String DRIVER = "db.driver";
+	private final String PASSWORD = "db.password";
     @Bean
-    public DataSource dataSource() {
+    public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test?useSSL=false");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(environment.getProperty(URL));
+        dataSource.setUsername(environment.getProperty(USER));
+        dataSource.setPassword(environment.getProperty(PASSWORD));
+        dataSource.setDriverClassName(environment.getProperty(DRIVER));
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/test?useSSL=false");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("root");
 
         return dataSource;
     }
     @Bean
-    public JdbcTemplate jdbcTemplate() {
+    public JdbcTemplate getJdbcTemplate() {
       JdbcTemplate jdbcTemplate = new JdbcTemplate();
-      jdbcTemplate.setDataSource(dataSource());
+      jdbcTemplate.setDataSource(getDataSource());
       return jdbcTemplate;
     }
 
     @Bean
-    public PersonDao personDAO(){
+    public PersonDao getPersonDao(){
     	PersonDao personDao = new PersonDaoImpl();
-      personDao.setJdbcTemplate(jdbcTemplate());
+       personDao.setJdbcTemplate(getJdbcTemplate());
       return personDao;
     }
-    @Bean
-    public PersonServiceImpl personServiceImpl()
+    @Bean(name="personService")
+    public PersonServiceImpl getPersonServiceImpl()
     {
     	PersonServiceImpl  personService=new PersonServiceImpl();
     	return personService;
